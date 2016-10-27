333ed2acc51ef58df5cf0cc54bb3d5b2b9cc76bb
pengkobe <526337223@qq.com>
2016-10-27 11:42:37 +0800

    init with webcomponent template(from jiongks.name)

---
+++ server/public/javascripts/index.js
@@ -0,0 +1,6 @@
+var addBox = document.querySelector('add-box');
+var todoList = document.querySelector('todo-list');
+
+addBox.addEventListener('add-item', function (e) {
+    todoList.addItem(e.detail);
+});
